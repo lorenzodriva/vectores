41a368e984e7829451d2928b4ccd5f187d84f45b
lorenzodriva <ldriva2004@gmail.com>
2024-06-05 11:20:32 -0300

    tercero vectores

--- aaaa.cpp
+++ aaaa.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <vector>
 
-usng namespace std;
+using namespace std;
 
 int main(){
-    vector<int> numerosDinamicos;
-    int numerosEstaticos[] = {1,2,3,4,5};
+    vector<int> numerosDinamicos = {1,2,3,4,5}; //inicializa el array (estatico)
+
+    cout << numerosDinamicos[3]<<endl; //muestra un elemento del array
+
+    numerosDinamicos.push_back(8); //agrega un elemento al final
+
+    size_t longitud = numerosDinamicos.size(); // calcula longitud del array
+
+    // bucle FOR normal
+    for (size_t i = 0; i < numerosDinamicos.size(); i++){ //IMPRIME CONTENIDO ARRAY
+        cout << numerosDinamicos[i] << " ";
+    }
+
+    cout<<endl;
+
+    //BASADO en rango
+    for (int value : numerosDinamicos){
+        cout << value << " ";
+    }
+
     return 0;
 }
